5a36e0223416aa0bb708d94cd638106e95e09119
Pawanvaree <156408317+Pawanvaree@users.noreply.github.com>
2024-03-01 10:04:41 +0000

    add

--- BST.h
+++ BST.h
@@ -137,7 +137,7 @@ void printTree( TreeNodePtr treePtr ,int l){
       l++;
       printTree( treePtr->rightPtr,l);
       for(i=1;i<l;i++)printf("    ");
-      printf("%3d\n",treePtr->data);
+      printf("%d\n",treePtr->data);
       printTree( treePtr->leftPtr,l);
    }
 }
